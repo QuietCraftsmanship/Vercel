QuietCraftsmanship/Vercel

commit 7cc7257a8ca67a42c72e8d2e29700decd856ac20

Set a default file mode for package.json

lib/index.js
@@ -129,17 +129,24 @@ export default class Now extends EventEmitter {
       // array has a group of files with the same sha but different path
       const files = await Promise.all(Array.prototype.concat.apply([], await Promise.all((Array.from(this._files)).map(async ([sha, {data, names}]) => {
         return await names.map(async name => {
-          if (this._static && toRelative(name, this._path) !== 'package.json') {
-            name = this.pathInsideContent(name)
-          }
+          let mode
 
-          const st = await (followSymlinks ? stat(name) : lstat(name))
+          if (this._static) {
+            if (toRelative(name, this._path) === 'package.json') {
+              mode = 33261
+            } else {
+              const st = await (followSymlinks ? stat(name) : lstat(name))
+              mode = st.mode
+
+              name = this.pathInsideContent(name)
+            }
+          }
 
           return {
             sha,
             size: data.length,
             file: toRelative(name, this._path),
-            mode: st.mode
+            mode
           }
         })
       }))))

commit b24d4c08721d43247218e68c4521bfb8b466d3f2

Fix file mode passing for regular deployments (#214)
Commit 7cc7257a8ca67a42c72e8d2e29700decd856ac20 broke file modes
for regular deployments as file mode isn't retrieved anymore.

lib/index.js
@@ -128,18 +128,24 @@ module.exports = class Now extends EventEmitter {
       // Flatten the array to contain files to sync where each nested input
       // array has a group of files with the same sha but different path
       const files = await Promise.all(Array.prototype.concat.apply([], await Promise.all((Array.from(this._files)).map(async ([sha, {data, names}]) => {
+        const statFn = followSymlinks ? stat : lstat
         return await names.map(async name => {
           let mode
+
+          const getMode = async () => {
+            const st = await statFn(name)
+            return st.mode
+          }
 
           if (this._static) {
             if (toRelative(name, this._path) === 'package.json') {
               mode = 33261
             } else {
-              const st = await (followSymlinks ? stat(name) : lstat(name))
-              mode = st.mode
-
+              mode = await getMode()
               name = this.pathInsideContent(name)
             }
+          } else {
+            mode = await getMode()
           }
 
           return {