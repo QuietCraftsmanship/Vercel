QuietCraftsmanship/Vercel

commit 357bef9f32456f725a24b239060bfe4ae44144b4

[now-python] Skip reinstalling user dependencies for `now dev` (#3352)
Same as #2926 but for python
Fixes #3351

packages/now-python/src/install.ts
@@ -1,0 +1,120 @@
+import execa from 'execa';
+import { debug, Meta } from '@now/build-utils';
+const pipPath = 'pip3';
+
+async function isInstalled(dependency: string, cwd: string) {
+  try {
+    await execa('python3', ['-c', `"import ${dependency}"`], {
+      stdio: 'pipe',
+      cwd,
+    });
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+const makeRequirementsCheckCode = (requirementsPath: string) => `
+import distutils.text_file
+import pkg_resources
+from pkg_resources import DistributionNotFound, VersionConflict
+dependencies = distutils.text_file.TextFile(filename='${requirementsPath}').readlines()
+pkg_resources.require(dependencies)
+`;
+
+async function areRequirementsInstalled(requirementsPath: string, cwd: string) {
+  try {
+    await execa(
+      'python3',
+      ['-c', makeRequirementsCheckCode(requirementsPath)],
+      {
+        stdio: 'pipe',
+        cwd,
+      }
+    );
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function pipInstall(workPath: string, args: string[]) {
+  const target = '.';
+  // See: https://github.com/pypa/pip/issues/4222#issuecomment-417646535
+  //
+  // Disable installing to the Python user install directory, which is
+  // the default behavior on Debian systems and causes error:
+  //
+  // distutils.errors.DistutilsOptionError: can't combine user with
+  // prefix, exec_prefix/home, or install_(plat)base
+  process.env.PIP_USER = '0';
+  debug(
+    `Running "pip install --disable-pip-version-check --target ${target} --upgrade ${args.join(
+      ' '
+    )}"...`
+  );
+  try {
+    await execa(
+      pipPath,
+      [
+        'install',
+        '--disable-pip-version-check',
+        '--target',
+        target,
+        '--upgrade',
+        ...args,
+      ],
+      {
+        cwd: workPath,
+        stdio: 'pipe',
+      }
+    );
+  } catch (err) {
+    console.log(
+      `Failed to run "pip install --disable-pip-version-check --target ${target} --upgrade ${args.join(
+        ' '
+      )}"...`
+    );
+    throw err;
+  }
+}
+
+interface InstallRequirementArg {
+  dependency: string;
+  workPath: string;
+  meta: Meta;
+  args?: string[];
+}
+
+export async function installRequirement({
+  dependency,
+  workPath,
+  meta,
+  args = [],
+}: InstallRequirementArg) {
+  if (meta.isDev || !(await isInstalled(dependency, workPath))) {
+    debug(`Skipping ${dependency} dependency installation, already installed`);
+    return;
+  }
+  await pipInstall(workPath, [dependency, ...args]);
+}
+
+interface InstallRequirementsFileArg {
+  filePath: string;
+  workPath: string;
+  meta: Meta;
+  args?: string[];
+}
+
+export async function installRequirementsFile({
+  filePath,
+  workPath,
+  meta,
+  args = [],
+}: InstallRequirementsFileArg) {
+  if (meta.isDev && (await areRequirementsInstalled(filePath, workPath))) {
+    debug(`Skipping requirements file installation, already installed`);
+    return;
+  }
+  await pipInstall(workPath, ['-r', filePath, ...args]);
+}

packages/now-python/src/index.ts
@@ -13,47 +13,7 @@ import {
   BuildOptions,
   debug,
 } from '@now/build-utils';
-
-async function pipInstall(pipPath: string, workDir: string, ...args: string[]) {
-  const target = '.';
-  // See: https://github.com/pypa/pip/issues/4222#issuecomment-417646535
-  //
-  // Disable installing to the Python user install directory, which is
-  // the default behavior on Debian systems and causes error:
-  //
-  // distutils.errors.DistutilsOptionError: can't combine user with
-  // prefix, exec_prefix/home, or install_(plat)base
-  process.env.PIP_USER = '0';
-  debug(
-    `Running "pip install --disable-pip-version-check --target ${target} --upgrade ${args.join(
-      ' '
-    )}"...`
-  );
-  try {
-    await execa(
-      pipPath,
-      [
-        'install',
-        '--disable-pip-version-check',
-        '--target',
-        target,
-        '--upgrade',
-        ...args,
-      ],
-      {
-        cwd: workDir,
-        stdio: 'pipe',
-      }
-    );
-  } catch (err) {
-    console.log(
-      `Failed to run "pip install --disable-pip-version-check --target ${target} --upgrade ${args.join(
-        ' '
-      )}"...`
-    );
-    throw err;
-  }
-}
+import { installRequirement, installRequirementsFile } from './install';
 
 async function pipenvConvert(cmd: string, srcDir: string) {
   debug('Running pipfile2req...');
@@ -70,16 +30,17 @@ async function pipenvConvert(cmd: string, srcDir: string) {
 
 export const version = 3;
 
-export const build = async ({
-  workPath,
-  files: originalFiles,
+export async function downloadFilesInWorkPath({
   entrypoint,
-  meta = {},
+  workPath,
+  files,
+  meta,
   config,
-}: BuildOptions) => {
-  let downloadedFiles = await download(originalFiles, workPath, meta);
 
-  if (meta.isDev) {
+}: BuildOptions) {
+    debug('Downloading user files...');
+    let downloadedFiles = await download(files, workPath, meta);
+    if (meta && meta.isDev) {
     let base = null;
 
     if (config && config.zeroConfig) {
@@ -93,8 +54,23 @@ export const build = async ({
     downloadedFiles = await glob('**', destNow);
     workPath = destNow;
   }
+  return workPath;
+}
 
-  const pipPath = 'pip3';
+export const build = async ({
+  workPath,
+  files: originalFiles,
+  entrypoint,
+  meta = {},
+  config,
+}: BuildOptions) => {
+  workPath = await downloadFilesInWorkPath({
+    workPath,
+    files: originalFiles,
+    entrypoint,
+    meta,
+    config,
+  });
 
   try {
     // See: https://stackoverflow.com/a/44728772/376773
@@ -113,8 +89,13 @@ export const build = async ({
     throw err;
   }
 
-  console.log('Installing dependencies...');
-  await pipInstall(pipPath, workPath, 'werkzeug');
+  console.log('Installing required dependencies...');
+
+  await installRequirement({
+    dependency: 'werkzeug',
+    workPath,
+    meta,
+  });
 
   let fsFiles = await glob('**', workPath);
   const entryDirectory = dirname(entrypoint);
@@ -133,12 +114,12 @@ export const build = async ({
     // to not be part of the lambda environment. By using pip's `--target` directive we can isolate
     // it into a separate folder.
     const tempDir = await getWriteableDirectory();
-    await pipInstall(
-      pipPath,
-      tempDir,
-      'pipfile-requirements',
-      '--no-warn-script-location'
-    );
+    await installRequirement({
+      dependency: 'pipfile-requirements',
+      workPath: tempDir,
+      meta,
+      args: ['--no-warn-script-location'],
+    });
 
     // Python needs to know where to look up all the packages we just installed.
     // We tell it to use the same location as used with `--target`
@@ -153,11 +134,19 @@ export const build = async ({
   if (fsFiles[requirementsTxt]) {
     debug('Found local "requirements.txt"');
     const requirementsTxtPath = fsFiles[requirementsTxt].fsPath;
-    await pipInstall(pipPath, workPath, '-r', requirementsTxtPath);
+    await installRequirementsFile({
+      filePath: requirementsTxtPath,
+      workPath,
+      meta,
+    });
   } else if (fsFiles['requirements.txt']) {
     debug('Found global "requirements.txt"');
     const requirementsTxtPath = fsFiles['requirements.txt'].fsPath;
-    await pipInstall(pipPath, workPath, '-r', requirementsTxtPath);
+    await installRequirementsFile({
+      filePath: requirementsTxtPath,
+      workPath,
+      meta,
+    });
   }
 
   const originalPyPath = join(__dirname, '..', 'now_init.py');
@@ -197,3 +186,6 @@ export const build = async ({
 };
 
 export { shouldServe };
+
+// internal only - expect breaking changes if other packages depend on these exports
+export { installRequirement, installRequirementsFile };

commit 3e79a292199c329c808a3500bd8baecf74902e76

[now-python] Fix dependency detection in now dev (#3695)
Fixes a false positive regression from #3352 where `now dev`  assumed that local dependencies were already installed even when they were not.

packages/now-python/src/install.ts
@@ -2,13 +2,24 @@ import execa from 'execa';
 import { debug, Meta } from '@now/build-utils';
 const pipPath = 'pip3';
 
+const makeDependencyCheckCode = (dependency: string) => `
+from importlib import util
+dep = '${dependency}'.replace('-', '_')
+spec = util.find_spec(dep)
+print(spec.origin)
+`;
+
 async function isInstalled(dependency: string, cwd: string) {
   try {
-    await execa('python3', ['-c', `"import ${dependency}"`], {
-      stdio: 'pipe',
-      cwd,
-    });
-    return true;
+    const { stdout } = await execa(
+      'python3',
+      ['-c', makeDependencyCheckCode(dependency)],
+      {
+        stdio: 'pipe',
+        cwd,
+      }
+    );
+    return stdout.startsWith(cwd);
   } catch (err) {
     return false;
   }
@@ -92,8 +103,10 @@ export async function installRequirement({
   meta,
   args = [],
 }: InstallRequirementArg) {
-  if (meta.isDev || !(await isInstalled(dependency, workPath))) {
-    debug(`Skipping ${dependency} dependency installation, already installed`);
+  if (meta.isDev && (await isInstalled(dependency, workPath))) {
+    debug(
+      `Skipping ${dependency} dependency installation, already installed in ${workPath}`
+    );
     return;
   }
   await pipInstall(workPath, [dependency, ...args]);