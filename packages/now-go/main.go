QuietCraftsmanship/Vercel

commit 6d9c40010b15f2b1c96e10b818432e7cd3b2c401

[all] Rename GitHub repo to vercel/vercel (#4506)
We renamed the GitHub repository from `zeit/now` to `vercel/vercel` so this PR updates all references to the repo URL.

There were also a few remaining references to Now CLI that have been updated to Vercel CLI.

packages/now-go/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"net/http"
-	now "github.com/zeit/now-go-bridge/go/bridge"
+	vc "github.com/vercel/go-bridge/go/bridgee"
 )
 
 func main() {
-	now.Start(http.HandlerFunc(__NOW_HANDLER_FUNC_NAME))
+	vc.Start(http.HandlerFunc(__NOW_HANDLER_FUNC_NAME))
 }

commit a8626a9ae715ed0241a4c6bdeb86c4939fddd726

[go] Fix typo in `go-bridge` import (#4578)
Introduced in 6d9c40010.

packages/now-go/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-	vc "github.com/vercel/go-bridge/go/bridgee"
+	vc "github.com/vercel/go-bridge/go/bridge"
 )
 
 func main() {