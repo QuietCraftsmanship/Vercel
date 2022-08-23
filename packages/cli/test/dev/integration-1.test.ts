QuietCraftsmanship/Vercel

commit fc3fa61b593265ae0d1519761142bc79154746b5

update edge-runtime to allow `instanceof` to work with primitives (#8242)
* update edge-runtime to allow `instanceof` to work with primitives

* finishin upgrading edge-runtime

* update to latest

* fix merge

* remove dev only from test

packages/node/src/dev-server.ts
@@ -76,7 +76,8 @@ import { getVercelLauncher } from '@vercel/node-bridge/launcher.js';
 import { VercelProxyResponse } from '@vercel/node-bridge/types';
 import { Config, streamToBuffer, debug } from '@vercel/build-utils';
 import exitHook from 'exit-hook';
-import { EdgeRuntime, Primitives, runServer } from 'edge-runtime';
+import { EdgeRuntime, runServer } from 'edge-runtime';
+import type { EdgeContext } from '@edge-runtime/vm';
 import { getConfig } from '@vercel/static-config';
 import { Project } from 'ts-morph';
 import esbuild from 'esbuild';
@@ -241,22 +242,19 @@ async function createEdgeRuntime(params?: {
     const wasmBindings = await params.wasmAssets.getContext();
     const edgeRuntime = new EdgeRuntime({
       initialCode: params.userCode,
-      extend: (context: Primitives) => {
-        Object.assign(
-          context,
-          {
-            // This is required for esbuild wrapping logic to resolve
-            module: {},
+      extend: (context: EdgeContext) => {
+        Object.assign(context, {
+          // This is required for esbuild wrapping logic to resolve
+          module: {},
 
-            // This is required for environment variable access.
-            // In production, env var access is provided by static analysis
-            // so that only the used values are available.
-            process: {
-              env: process.env,
-            },
+          // This is required for environment variable access.
+          // In production, env var access is provided by static analysis
+          // so that only the used values are available.
+          process: {
+            env: process.env,
           },
-          wasmBindings
-        );
+          wasmBindings,
+        });
 
         return context;
       },

packages/cli/test/dev/integration-1.test.ts
@@ -43,8 +43,8 @@ test('[vercel dev] should support edge functions', async () => {
       url: `http://localhost:${port}/api/edge-success`,
       method: 'POST',
       body: '{"hello":"world"}',
-      decamelized: 'some_camel_case_thing',
-      uppercase: 'SOMETHING',
+      snakeCase: 'some_camel_case_thing',
+      upperCase: 'SOMETHING',
       optionalChaining: 'fallback',
       ENV_VAR_IN_EDGE: '1',
     });

packages/cli/test/dev/fixtures/edge-function/api/edge-success.js
@@ -1,4 +1,4 @@
-import decamelize from 'decamelize';
+import { snakeCase } from 'snake-case';
 import { upper } from '../lib/upper';
 
 export const config = {
@@ -14,8 +14,8 @@ export default async function edge(request, event) {
       url: request.url,
       method: request.method,
       body: requestBody,
-      decamelized: decamelize('someCamelCaseThing'),
-      uppercase: upper('someThing'),
+      snakeCase: snakeCase('someCamelCaseThing'),
+      upperCase: upper('someThing'),
       optionalChaining: request?.doesnotexist ?? 'fallback',
       ENV_VAR_IN_EDGE: process.env.ENV_VAR_IN_EDGE,
     })

commit 0726ce1432aa88944720168eafe47e832cbb83ce

[node] fix WebAssembly bindings evaluating to `undefined` (#8417)
This was broken since https://github.com/vercel/vercel/pull/8242, which accidentally pushed all WebAssembly modules to be nested under `global.wasmBindings` instead of being directly under the global scope.

### Related Issues

- This was broken since https://github.com/vercel/vercel/pull/8242

### 📋 Checklist

<!--
  Please keep your PR as a Draft until the checklist is complete
-->

#### Tests

- [ ] The code changed/added as part of this PR has been covered with tests
- [ ] All tests pass locally with `yarn test-unit`

#### Code Review

- [ ] This PR has a concise title and thorough description useful to a reviewer
- [ ] Issue from task tracker has a link to this PR

packages/node/src/dev-server.ts
@@ -253,7 +253,9 @@ async function createEdgeRuntime(params?: {
           process: {
             env: process.env,
           },
-          wasmBindings,
+
+          // These are the global bindings for WebAssembly module
+          ...wasmBindings,
         });
 
         return context;

packages/cli/test/dev/fixtures/edge-function/api/webassembly.js
@@ -1,0 +1,13 @@
+import mod from '../increment.wasm?module';
+
+export const config = { runtime: 'experimental-edge' };
+
+const init$ = WebAssembly.instantiate(mod);
+
+/** @param {Request} req */
+export default async req => {
+  const givenNumber = Number(new URL(req.url).searchParams.get('number') || 0);
+  const { exports } = await init$;
+  const added = exports.add_one(givenNumber);
+  return new Response(`${givenNumber} + 1 = ${added}`);
+};

packages/cli/test/dev/integration-1.test.ts
@@ -53,6 +53,33 @@ test('[vercel dev] should support edge functions', async () => {
   }
 });
 
+test('[vercel dev] edge functions support WebAssembly files', async () => {
+  const dir = fixture('edge-function');
+  const { dev, port, readyResolver } = await testFixture(dir, {
+    env: {
+      ENV_VAR_IN_EDGE: '1',
+    },
+  });
+
+  try {
+    await readyResolver;
+
+    for (const { number, result } of [
+      { number: 1, result: 2 },
+      { number: 2, result: 3 },
+      { number: 12, result: 13 },
+    ]) {
+      let res = await fetch(
+        `http://localhost:${port}/api/webassembly?number=${number}`
+      );
+      validateResponseHeaders(res);
+      await expect(res.text()).resolves.toEqual(`${number} + 1 = ${result}`);
+    }
+  } finally {
+    await dev.kill('SIGTERM');
+  }
+});
+
 test(
   '[vercel dev] edge functions respond properly the same as production',
   testFixtureStdio('edge-function', async (testPath: any) => {