QuietCraftsmanship/Vercel

commit a17d1f4a8bf112c26c3a50dbfe562937c27326e6

[cli] Support Custom Environments in `vercel env add [target]` (#11994)

packages/cli/src/commands/env/add.ts
@@ -1,12 +1,11 @@
 import chalk from 'chalk';
-import type { Project, ProjectEnvTarget } from '@vercel-internals/types';
+import type { Project } from '@vercel-internals/types';
 import { Output } from '../../util/output';
 import Client from '../../util/client';
 import stamp from '../../util/output/stamp';
 import addEnvRecord from '../../util/env/add-env-record';
 import getEnvRecords from '../../util/env/get-env-records';
 import {
-  isValidEnvTarget,
   getEnvTargetPlaceholder,
   envTargetChoices,
 } from '../../util/env/env-target';
@@ -16,6 +15,7 @@ import { emoji, prependEmoji } from '../../util/emoji';
 import { isKnownError } from '../../util/env/known-error';
 import { getCommandName } from '../../util/pkg-name';
 import { isAPIError } from '../../util/errors-ts';
+import { getCustomEnvironments } from '../../util/target/get-custom-environments';
 
 type Options = {
   '--debug': boolean;
@@ -51,36 +51,48 @@ export default async function add(
     return 1;
   }
 
-  let envTargets: ProjectEnvTarget[] = [];
+  let envTargets: string[] = [];
   if (envTargetArg) {
-    if (!isValidEnvTarget(envTargetArg)) {
-      output.error(
-        `The Environment ${param(
-          envTargetArg
-        )} is invalid. It must be one of: ${getEnvTargetPlaceholder()}.`
-      );
-      return 1;
-    }
     envTargets.push(envTargetArg);
   }
 
   if (!envName) {
     envName = await client.input.text({
-      message: `What’s the name of the variable?`,
+      message: `What's the name of the variable?`,
       validate: val => (val ? true : 'Name cannot be empty'),
     });
   }
 
-  const { envs } = await getEnvRecords(
-    output,
-    client,
-    project.id,
-    'vercel-cli:env:add'
-  );
-  const existing = new Set(
-    envs.filter(r => r.key === envName).map(r => r.target)
-  );
-  const choices = envTargetChoices.filter(c => !existing.has(c.value));
+  const [{ envs }, customEnvironments] = await Promise.all([
+    getEnvRecords(output, client, project.id, 'vercel-cli:env:add'),
+    getCustomEnvironments(client, project.id),
+  ]);
+  const matchingEnvs = envs.filter(r => r.key === envName);
+  const existingTargets = new Set<string>();
+  const existingCustomEnvs = new Set<string>();
+  for (const env of matchingEnvs) {
+    if (typeof env.target === 'string') {
+      existingTargets.add(env.target);
+    } else if (Array.isArray(env.target)) {
+      for (const target of env.target) {
+        existingTargets.add(target);
+      }
+    }
+    if (env.customEnvironmentIds) {
+      for (const customEnvId of env.customEnvironmentIds) {
+        existingCustomEnvs.add(customEnvId);
+      }
+    }
+  }
+  const choices = [
+    ...envTargetChoices.filter(c => !existingTargets.has(c.value)),
+    ...customEnvironments
+      .filter(c => !existingCustomEnvs.has(c.id))
+      .map(c => ({
+        name: c.name,
+        value: c.id,
+      })),
+  ];
 
   if (choices.length === 0 && !opts['--force']) {
     output.error(
@@ -99,7 +111,7 @@ export default async function add(
     envValue = stdInput;
   } else {
     envValue = await client.input.text({
-      message: `What’s the value of ${envName}?`,
+      message: `What's the value of ${envName}?`,
     });
   }
 

packages/cli/test/unit/commands/env.test.ts
@@ -252,7 +252,7 @@ describe('env', () => {
         client.setArgv('env', 'add', 'NEW_VAR');
         const addPromise = env(client);
 
-        await expect(client.stderr).toOutput('What’s the value of NEW_VAR?');
+        await expect(client.stderr).toOutput("What's the value of NEW_VAR?");
         client.stdin.write('testvalue\n');
 
         await expect(client.stderr).toOutput(

packages/cli/src/commands/env/command.ts
@@ -17,7 +17,12 @@ export const envCommand = {
     {
       name: 'ls',
       description: 'List all variables for the specified Environment',
-      arguments: [],
+      arguments: [
+        {
+          name: 'environment',
+          required: false,
+        },
+      ],
       options: [],
       examples: [],
     },

packages/cli/src/commands/env/ls.ts
@@ -14,7 +14,7 @@ import { getEnvTargetPlaceholder } from '../../util/env/env-target';
 import stamp from '../../util/output/stamp';
 import { getCommandName } from '../../util/pkg-name';
 import ellipsis from '../../util/output/ellipsis';
-import { isObject } from '@vercel/error-utils';
+import { getCustomEnvironments } from '../../util/target/get-custom-environments';
 
 type Options = {};
 
@@ -63,22 +63,6 @@ export default async function ls(
   }
 
   return 0;
-}
-
-async function getCustomEnvironments(client: Client, projectId: string) {
-  try {
-    const res = await client.fetch<{ environments: CustomEnvironment[] }>(
-      `/projects/${encodeURIComponent(projectId)}/custom-environments`,
-      { method: 'GET' }
-    );
-    return res.environments;
-  } catch (error) {
-    if (isObject(error) && error.status === 404) {
-      // user is not flagged for custom environments
-      return [];
-    }
-    throw error;
-  }
 }
 
 function getTable(

packages/cli/src/commands/target/list.ts
@@ -4,7 +4,7 @@ import table from '../../util/output/table';
 import Client from '../../util/client';
 import { getCommandName } from '../../util/pkg-name';
 import type { ProjectLinked } from '@vercel-internals/types';
-import type { CustomEnvironment } from '../../util/target/types';
+import type { CustomEnvironment } from '@vercel-internals/types';
 
 export default async function list(
   client: Client,

packages/cli/src/util/env/add-env-record.ts
@@ -5,6 +5,7 @@ import type {
   ProjectEnvVariable,
   ProjectEnvType,
 } from '@vercel-internals/types';
+import { PROJECT_ENV_TARGET } from '@vercel-internals/constants';
 
 export default async function addEnvRecord(
   output: Output,
@@ -14,18 +15,28 @@ export default async function addEnvRecord(
   type: ProjectEnvType,
   key: string,
   value: string,
-  targets: ProjectEnvTarget[],
+  targets: string[],
   gitBranch: string
 ): Promise<void> {
   const actionWord = upsert ? 'Overriding' : 'Adding';
   output.debug(
     `${actionWord} ${type} Environment Variable ${key} to ${targets.length} targets`
   );
+  const target: ProjectEnvTarget[] = [];
+  const customEnvironmentIds: string[] = [];
+  for (const t of targets) {
+    const arr = PROJECT_ENV_TARGET.includes(t as ProjectEnvTarget)
+      ? target
+      : customEnvironmentIds;
+    arr.push(t);
+  }
   const body: Omit<ProjectEnvVariable, 'id'> = {
     type,
     key,
     value,
-    target: targets,
+    target,
+    customEnvironmentIds:
+      customEnvironmentIds.length > 0 ? customEnvironmentIds : undefined,
     gitBranch: gitBranch || undefined,
   };
   const args = upsert ? `?upsert=${upsert}` : '';

packages/cli/src/util/env/get-env-records.ts
@@ -1,10 +1,7 @@
+import { URLSearchParams } from 'url';
 import { Output } from '../output';
 import Client from '../client';
-import type {
-  ProjectEnvVariable,
-  ProjectEnvTarget,
-} from '@vercel-internals/types';
-import { URLSearchParams } from 'url';
+import type { ProjectEnvVariable } from '@vercel-internals/types';
 
 /** The CLI command that was used that needs the environment variables. */
 export type EnvRecordsSource =
@@ -25,7 +22,7 @@ export default async function getEnvRecords(
     gitBranch,
     decrypt,
   }: {
-    target?: ProjectEnvTarget | string;
+    target?: string;
     gitBranch?: string;
     decrypt?: boolean;
   } = {}
@@ -62,7 +59,7 @@ export default async function getEnvRecords(
 }
 
 interface PullEnvOptions {
-  target?: ProjectEnvTarget | string;
+  target?: string;
   gitBranch?: string;
 }
 

packages/cli/src/util/target/get-custom-environments.ts
@@ -1,0 +1,19 @@
+import Client from '../client';
+import { isObject } from '@vercel/error-utils';
+import { CustomEnvironment } from '@vercel-internals/types';
+
+export async function getCustomEnvironments(client: Client, projectId: string) {
+  try {
+    const res = await client.fetch<{ environments: CustomEnvironment[] }>(
+      `/projects/${encodeURIComponent(projectId)}/custom-environments`,
+      { method: 'GET' }
+    );
+    return res.environments;
+  } catch (error) {
+    if (isObject(error) && error.status === 404) {
+      // user is not flagged for custom environments
+      return [];
+    }
+    throw error;
+  }
+}

commit a756c80f7763204699ffe91ccf7a58c9c20ac037

[cli] Fix gitBranch parameter for `vc env add` command (#12085)
The gitBranch parameter was inadvertently broken in #11994.

packages/cli/src/commands/env/add.ts
@@ -94,7 +94,7 @@ export default async function add(
       })),
   ];
 
-  if (choices.length === 0 && !opts['--force']) {
+  if (!envGitBranch && choices.length === 0 && !opts['--force']) {
     output.error(
       `The variable ${param(
         envName

packages/cli/test/mocks/project.ts
@@ -276,7 +276,7 @@ export function useProject(
         ? parseEnvironment(req.query.target)
         : undefined;
 
-    let targetEnvs = envs;
+    let targetEnvs = projectEnvs;
     if (target) {
       targetEnvs = targetEnvs.filter(env => {
         if (typeof env.target === 'string') {

packages/cli/test/unit/commands/env.test.ts
@@ -10,6 +10,51 @@ import { useTeams } from '../../mocks/team';
 import { useUser } from '../../mocks/user';
 
 describe('env', () => {
+  describe('add', () => {
+    it('should allow `gitBranch` to be passed', async () => {
+      useUser();
+      useTeams('team_dummy');
+      useProject(
+        {
+          ...defaultProject,
+          id: 'vercel-env-pull',
+          name: 'vercel-env-pull',
+        },
+        [
+          ...envs,
+          {
+            type: 'encrypted',
+            id: '781dt89g8r2h789g',
+            key: 'REDIS_CONNECTION_STRING',
+            value: 'redis://abc123@redis.example.dev:6379',
+            target: ['development'],
+            gitBranch: undefined,
+            configurationId: null,
+            updatedAt: 1557241361455,
+            createdAt: 1557241361455,
+          },
+        ]
+      );
+      const cwd = setupUnitFixture('vercel-env-pull');
+      client.cwd = cwd;
+      client.setArgv(
+        'env',
+        'add',
+        'REDIS_CONNECTION_STRING',
+        'preview',
+        'branchName'
+      );
+      const exitCodePromise = env(client);
+      await expect(client.stderr).toOutput(
+        "What's the value of REDIS_CONNECTION_STRING?"
+      );
+      client.stdin.write('testvalue\n');
+      await expect(client.stderr).toOutput(
+        'Added Environment Variable REDIS_CONNECTION_STRING to Project vercel-env-pull'
+      );
+      await expect(exitCodePromise).resolves.toEqual(0);
+    });
+  });
   describe('pull', () => {
     it('should handle pulling', async () => {
       useUser();