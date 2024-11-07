QuietCraftsmanship/Vercel

commit fcc4fa817f1f89aae47c596e304c0ced9bbb316b

[cli] Replace `psl` with `tldts` (#12174)
`psl` is one of the deps causing the `punycode` deprecation warning on
Node 22. [The newest version includes the deprecation
warning](https://github.com/lupomontero/psl/issues/323), so we can't
just update `psl`.

Inspired by https://github.com/salesforce/tough-cookie/pull/346.

packages/cli/src/commands/certs/issue.ts
@@ -1,4 +1,4 @@
-import { parse } from 'psl';
+import { getSubdomain } from 'tldts';
 import chalk from 'chalk';
 
 import { Output } from '../../util/output';
@@ -175,14 +175,12 @@ async function runStartOrder(
   );
   const [header, ...rows] = dnsTable(
     pendingChallenges.map(challenge => {
-      const parsedDomain = parse(challenge.domain);
-      if (parsedDomain.error) {
+      const subdomain = getSubdomain(challenge.domain);
+      if (!subdomain) {
         throw new ERRORS.InvalidDomain(challenge.domain);
       }
       return [
-        parsedDomain.subdomain
-          ? `_acme-challenge.${parsedDomain.subdomain}`
-          : `_acme-challenge`,
+        subdomain ? `_acme-challenge.${subdomain}` : `_acme-challenge`,
         'TXT',
         challenge.value,
       ];

packages/cli/src/commands/domains/buy.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
-import psl from 'psl';
+import { parse } from 'tldts';
 
 import * as ERRORS from '../../util/errors-ts';
 import Client from '../../util/client';
@@ -33,11 +33,7 @@ export default async function buy(
     return 1;
   }
 
-  const parsedDomain = psl.parse(domainName);
-  if (parsedDomain.error) {
-    output.error(`The provided domain name ${param(domainName)} is invalid`);
-    return 1;
-  }
+  const parsedDomain = parse(domainName);
 
   const { domain: rootDomain, subdomain } = parsedDomain;
   if (subdomain || !rootDomain) {

packages/cli/src/util/certs/get-wildcard-cns-for-alias.ts
@@ -1,4 +1,4 @@
-import psl from 'psl';
+import { parse } from 'tldts';
 import { InvalidDomain } from '../errors-ts';
 import isWildcardAlias from '../alias/is-wildcard-alias';
 import extractDomain from '../alias/extract-domain';
@@ -8,10 +8,7 @@ export default function getWildcardCNSForAlias(alias: string) {
     return [extractDomain(alias), alias];
   }
 
-  const parsedDomain = psl.parse(alias);
-  if (parsedDomain.error) {
-    throw new InvalidDomain(alias);
-  }
+  const parsedDomain = parse(alias);
 
   const { domain, subdomain } = parsedDomain;
   if (!domain) {

packages/cli/src/util/certs/handle-cert-error.ts
@@ -1,5 +1,5 @@
 import ms from 'ms';
-import { parse } from 'psl';
+import { parse } from 'tldts';
 import chalk from 'chalk';
 import * as ERRORS from '../errors-ts';
 import { Output } from '../output';
@@ -55,7 +55,7 @@ export default function handleCertError<T>(
         `${dnsTable(
           cns.map(cn => {
             const parsed = parse(cn);
-            return !parsed.error && parsed.subdomain
+            return parsed.subdomain
               ? [parsed.subdomain, 'ALIAS', 'alias.vercel.com']
               : ['', 'ALIAS', 'alias.vercel.com'];
           })

packages/cli/src/util/deploy/generate-cert-for-deploy.ts
@@ -1,4 +1,4 @@
-import psl from 'psl';
+import { parse } from 'tldts';
 import { NowError } from '../now-error';
 import Client from '../client';
 import createCertForCns from '../certs/create-cert-for-cns';
@@ -11,11 +11,7 @@ export default async function generateCertForDeploy(
   deployURL: string
 ) {
   const { output } = client;
-  const parsedDomain = psl.parse(deployURL);
-  if (parsedDomain.error) {
-    return new InvalidDomain(deployURL, parsedDomain.error.message);
-  }
-
+  const parsedDomain = parse(deployURL);
   const { domain } = parsedDomain;
   if (!domain) {
     return new InvalidDomain(deployURL);

packages/cli/src/util/domains/setup-domain.ts
@@ -1,4 +1,4 @@
-import psl from 'psl';
+import { parse } from 'tldts';
 import { NowError } from '../now-error';
 import type { Domain } from '@vercel-internals/types';
 import { Output } from '../output';
@@ -45,10 +45,7 @@ export default async function setupDomain(
     output.debug(
       `Domain ${aliasDomain} is not available to be purchased. Trying to add it`
     );
-    const parsedDomain = psl.parse(aliasDomain);
-    if (parsedDomain.error) {
-      return new ERRORS.InvalidDomain(alias, parsedDomain.error.message);
-    }
+    const parsedDomain = parse(aliasDomain);
     if (!parsedDomain.domain) {
       return new ERRORS.InvalidDomain(alias);
     }

packages/cli/src/util/is-root-domain.ts
@@ -1,10 +1,7 @@
-import psl from 'psl';
+import { parse } from 'tldts';
 
 export default function isRootDomain(domainName: string) {
-  const parsedDomain = psl.parse(domainName);
-  if (parsedDomain.error) {
-    return false;
-  }
+  const parsedDomain = parse(domainName);
   const { domain: rootDomain, subdomain } = parsedDomain;
   return Boolean(!subdomain && rootDomain);
 }

pnpm-lock.yaml
@@ -460,9 +460,6 @@ importers:
       '@types/pluralize':
         specifier: 0.0.29
         version: 0.0.29
-      '@types/psl':
-        specifier: 1.1.0
-        version: 1.1.0
       '@types/qs':
         specifier: 6.9.7
         version: 6.9.7
@@ -679,9 +676,6 @@ importers:
       proxy-agent:
         specifier: 6.3.0
         version: 6.3.0
-      psl:
-        specifier: 1.1.31
-        version: 1.1.31
       qr-image:
         specifier: 3.2.0
         version: 3.2.0
@@ -712,6 +706,9 @@ importers:
       title:
         specifier: 3.4.1
         version: 3.4.1
+      tldts:
+        specifier: 6.1.47
+        version: 6.1.47
       tmp-promise:
         specifier: 1.0.3
         version: 1.0.3
@@ -6182,10 +6179,6 @@ packages:
 
   /@types/prop-types@15.7.5:
     resolution: {integrity: sha512-JCB8C6SnDoQf0cNycqd/35A7MjcnK+ZTqE7judS6o7utxUCg6imJg3QK2qzHKszlTjcj2cn+NwMB2i96ubpj7w==}
-    dev: true
-
-  /@types/psl@1.1.0:
-    resolution: {integrity: sha512-HhZnoLAvI2koev3czVPzBNRYvdrzJGLjQbWZhqFmS9Q6a0yumc5qtfSahBGb5g+6qWvA8iiQktqGkwoIXa/BNQ==}
     dev: true
 
   /@types/qs@6.9.7:
@@ -15771,10 +15764,6 @@ packages:
     resolution: {integrity: sha512-b/YwNhb8lk1Zz2+bXXpS/LK9OisiZZ1SNsSLxN1x2OXVEhW2Ckr/7mWE5vrC1ZTiJlD9g19jWszTmJsB+oEpFQ==}
     dev: true
 
-  /psl@1.1.31:
-    resolution: {integrity: sha512-/6pt4+C+T+wZUieKR620OpzN/LlnNKuWjy1iFLQ/UG35JqHlR/89MP1d96dUfkf6Dne3TuLQzOYEYshJ+Hx8mw==}
-    dev: true
-
   /pump@1.0.3:
     resolution: {integrity: sha512-8k0JupWme55+9tCVE+FS5ULT3K6AbgqrGa58lTT49RpyfwwcGedHqaC5LlQNdEAumn/wFsu6aPwkuPMioy8kqw==}
     dependencies:
@@ -17319,6 +17308,17 @@ packages:
   /titleize@1.0.0:
     resolution: {integrity: sha512-TARUb7z1pGvlLxgPk++7wJ6aycXF3GJ0sNSBTAsTuJrQG5QuZlkUQP+zl+nbjAh4gMX9yDw9ZYklMd7vAfJKEw==}
     engines: {node: '>=0.10.0'}
+    dev: true
+
+  /tldts-core@6.1.47:
+    resolution: {integrity: sha512-6SWyFMnlst1fEt7GQVAAu16EGgFK0cLouH/2Mk6Ftlwhv3Ol40L0dlpGMcnnNiiOMyD2EV/aF3S+U2nKvvLvrA==}
+    dev: true
+
+  /tldts@6.1.47:
+    resolution: {integrity: sha512-R/K2tZ5MiY+mVrnSkNJkwqYT2vUv1lcT6wJvd2emGaMJ7PHUGRY4e3tUsdFCXgqxi2QgbHjL3yJgXCo40v9Hxw==}
+    hasBin: true
+    dependencies:
+      tldts-core: 6.1.47
     dev: true
 
   /tmp-promise@1.0.3:

commit 3423abe34a90f0e6dea01a3ac7e4fba02271cc86

[cli] Fix `vc certs issue` subdomain parsing (#12532)
I added a regression in
https://github.com/vercel/vercel/commit/fcc4fa817f1f89aae47c596e304c0ced9bbb316b#diff-4463dfbbb283602d090922c89d530aa8efb60baa260ffddcc33b275da6a3b00f
where domains without a subdomain would incorrectly throw an error.

This fixes that and adds a test that catches this case.

---------

Co-authored-by: Nathan Rajlich <n@n8.io>

packages/cli/src/commands/certs/issue.ts
@@ -199,7 +199,7 @@ async function runStartOrder(
   const [header, ...rows] = dnsTable(
     pendingChallenges.map(challenge => {
       const subdomain = getSubdomain(challenge.domain);
-      if (!subdomain) {
+      if (subdomain === null) {
         throw new ERRORS.InvalidDomain(challenge.domain);
       }
       return [

packages/cli/src/util/certs/create-cert-from-file.ts
@@ -19,7 +19,7 @@ export default async function createCertFromFile(
     const key = readFileSync(resolve(keyPath), 'utf8');
     const ca = readFileSync(resolve(caPath), 'utf8');
 
-    const certificate = await client.fetch<Cert>('/v3/now/certs', {
+    const certificate = await client.fetch<Cert>('/v3/certs', {
       method: 'PUT',
       body: {
         ca,

packages/cli/src/util/certs/finish-cert-order.ts
@@ -13,7 +13,7 @@ export default async function startCertOrder(
 ) {
   output.spinner(`Issuing a certificate for ${chalk.bold(cns.join(', '))}`);
   try {
-    const cert = await client.fetch<Cert>('/v3/now/certs', {
+    const cert = await client.fetch<Cert>('/v3/certs', {
       method: 'PATCH',
       body: {
         op: 'finalizeOrder',

packages/cli/src/util/certs/issue-cert.ts
@@ -10,7 +10,7 @@ export default async function issueCert(client: Client, cns: string[]) {
   return retry(
     async bail => {
       try {
-        return await client.fetch<Cert>('/v3/now/certs', {
+        return await client.fetch<Cert>('/v3/certs', {
           method: 'POST',
           body: { domains: cns },
         });

packages/cli/src/util/certs/start-cert-order.ts
@@ -29,7 +29,7 @@ export default async function startCertOrder(
       cns.join(', ')
     )} under ${chalk.bold(contextName)}`
   );
-  const order = await client.fetch<CertificateOrder>('/v3/now/certs', {
+  const order = await client.fetch<CertificateOrder>('/v3/certs', {
     method: 'PATCH',
     body: {
       op: 'startOrder',

packages/cli/test/unit/commands/certs/issue.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import { client } from '../../../mocks/client';
 import certs from '../../../../src/commands/certs';
+import { useUser } from '../../../mocks/user';
 
 describe('certs issue', () => {
   describe('--help', () => {
@@ -30,6 +31,24 @@ describe('certs issue', () => {
     await expect(exitCodePromise).resolves.toEqual(1);
   });
 
+  it('should issue cert', async () => {
+    useUser();
+    client.scenario.post('/v3/certs', (_, res) => {
+      return res.json({});
+    });
+    client.scenario.patch('/v3/certs', (_, res) => {
+      return res.json({ cns: ['acme.com'] });
+    });
+
+    client.setArgv('certs', 'issue', 'acme.com');
+    const exitCode = certs(client);
+    await expect(client.stderr).toOutput('Issuing a certificate for acme.com');
+    await expect(client.stderr).toOutput(
+      'Success! Certificate entry for acme.com created'
+    );
+    await expect(exitCode).resolves.toEqual(0);
+  });
+
   it('should track subcommand usage', async () => {
     client.setArgv('certs', 'issue');
     const exitCode = await certs(client);
@@ -57,6 +76,26 @@ describe('certs issue', () => {
           value: 'TRUE',
         },
       ]);
+    });
+
+    it('should handle challenges', async () => {
+      useUser();
+      client.scenario.post('/v3/certs', (_, res) => {
+        return res.json({});
+      });
+      client.scenario.patch('/v3/certs', (_, res) => {
+        return res.json({
+          cns: ['acme.com'],
+          challengesToResolve: [{ domain: 'acme.com', status: 'pending' }],
+        });
+      });
+
+      client.setArgv('certs', 'issue', 'acme.com', '--challenge-only');
+      const exitCode = certs(client);
+      await expect(client.stderr).toOutput(
+        'A certificate issuance for acme.com has been started'
+      );
+      await expect(exitCode).resolves.toEqual(0);
     });
   });
 